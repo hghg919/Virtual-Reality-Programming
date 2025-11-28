bd57dbf4e072c11a1586ec3011810b5c606d9252
hghg919 <tgeq1023@naver.com>
2025-11-28 13:04:46 +0900

    (2025.11.28) 12주차
    자살함

---
+++ 12주차/WPF_MoveScreen/WPF_MoveScreen/Final.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_MoveScreen
+{
+    /// <summary>
+    /// Final.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Final : Window
+    {
+        public Final()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInput_Click(object sender, RoutedEventArgs e)
+        {
+            Welcome welcome = new Welcome();
+            welcome.Show();
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
